b82ac6bc0519af8fe31499a97cb624cbdea657a8
Круглова Анна <154609892+absurdityinme@users.noreply.github.com>
2024-07-07 02:38:43 +0300

    grouprepository added

---
+++ app/src/main/java/com/app/GroupRepository.kt
@@ -0,0 +1,13 @@
+package com.app
+
+object GroupRepository {
+    var groups: MutableList<String> = mutableListOf(
+        "11-300",
+        "11-301",
+        "11-302",
+        "11-303",
+        "11-304",
+        "11-305",
+        "11-306"
+        )
+}
