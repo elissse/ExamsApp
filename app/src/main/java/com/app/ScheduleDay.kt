cc3461663ee8dec35744b29438883f25972d3492
Диана Ахмадуллина <diaakhmadullina@gmail.com>
2024-07-06 15:21:46 +0300

    заглушка на определении группы

--- app/src/main/java/com/app/ScheduleDay.kt
+++
@@ -1,7 +0,0 @@
-package com.app
-
-data class ScheduleDay (
-    val date: Int,
-    val dayOfWeek: String,
-    val subject: String
-)
--- app/src/main/java/com/app/ScheduleDayRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.app
-
-object ScheduleDayRepository {
-    val vinyls: List<ScheduleDay> = listOf(
-        ScheduleDay(1, "Суббота", "Алгем"),
-        ScheduleDay(2, "Воскресенье", ""),
-        ScheduleDay(3, "Понедельник", "")
-    )
-}
