97cdfd03e5f055ffc7f5a81e7be9460b289e3417
Круглова Анна <154609892+absurdityinme@users.noreply.github.com>
2024-07-07 02:39:08 +0300

    authorisation added

--- app/src/main/java/com/app/AccountFragment.kt
+++ app/src/main/java/com/app/AccountFragment.kt
@@ -6,6 +6,10 @@ import android.view.View
 import com.app.databinding.FragmentAccountBinding
 
 class AccountFragment : Fragment(R.layout.fragment_account) {
+    private var name: String? = null
+    private var surname: String? = null
+    private var patronymic: String? = null
+    private var group: String? = null
     private var binding: FragmentAccountBinding? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
