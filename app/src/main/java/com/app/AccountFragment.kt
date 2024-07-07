b599f330382cc0dc592349b6f4de4be864a037c1
Круглова Анна <154609892+absurdityinme@users.noreply.github.com>
2024-07-07 14:03:12 +0300

    unused import deleted

--- app/src/main/java/com/app/AccountFragment.kt
+++ app/src/main/java/com/app/AccountFragment.kt
@@ -1,7 +1,6 @@
 package com.app
 
 import android.content.Context
-import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
