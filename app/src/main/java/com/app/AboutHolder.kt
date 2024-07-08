318fab73cf01faba4281ce4c3eac5529534c3809
Круглова Анна <154609892+absurdityinme@users.noreply.github.com>
2024-07-08 18:56:28 +0300

    unused deleted

--- app/src/main/java/com/app/AboutHolder.kt
+++ app/src/main/java/com/app/AboutHolder.kt
@@ -1,20 +1,12 @@
 package com.app
 
 import android.content.Context
-import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.app.databinding.ItemAboutUsBinding
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.bumptech.glide.request.RequestOptions
 
 class AboutHolder(
     private val binding: ItemAboutUsBinding
 ): ViewHolder(binding.root) {
-
-    private val requestOptions = RequestOptions
-        .diskCacheStrategyOf(
-            DiskCacheStrategy.ALL
-        )
 
     private val context: Context
         get() = itemView.context
